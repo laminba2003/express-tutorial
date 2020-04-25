0d746e5be25a52d7760d71afeca51cb33d3e9d9b
mamadou lamine ba <laminba2003@gmail.com>
2020-04-25 14:15:27 +0000

    améliorations pour la lecture et l'envoi du fichier pays.json

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+const fs = require('fs');
 
 app.get('/', function (req, res) {
    res.send('Hello World');
 })
 
-var server = app.listen(8081, function () {
-   var host = server.address().address
-   var port = server.address().port
-   
-   console.log("Example app listening at http://%s:%s", host, port)
+app.get('/pays', function (req, res) {
+   fs.readFile('pays.json', 'utf8', function (erreur, donnees) {
+	if (erreur) throw erreur; 
+	res.send(donnees);
+  });
 })
 
-var monJson = require('pays.json');
-var chaine = '{ code: FR, pays: France }';
-var monJson = JSON.parse(chaine);
 
-var fs = require('fs');
-fs.readFile('pays.json', 'utf8', function (erreur, donnees)
-{
- if (erreur)
- throw erreur; 
- var monJson = JSON.parse(donnees);
+const server = app.listen(8082, function () {
+   const host = server.address().address
+   const port = server.address().port
+   console.log("Example app listening at http://%s:%s", host, port)
 });
+
