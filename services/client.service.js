11798b6a583079ddef76356613fe9c499765e754
maremend <mareme.ndoye@hotmail.fr>
2020-05-02 14:02:58 +0200

    up

---
+++ services/client.service.js
@@ -0,0 +1,18 @@
+import { Client } from "../models/client";
+
+export class PaysService {
+    
+    getClient() {
+        const client = [];
+        const client1 = new Client();
+        client1.nom = "thomson";
+        client1.prenom = "Gabriel";
+        client.push(client1);
+        const client2 = new Client();
+        client2.nom = "Ford";
+        client2.prenom = "Taraji";
+        client.push(client2);
+        return client;
+    }
+
+}
