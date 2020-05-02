08ff416e1b492276966e7ea48007f6e41ee2e8eb
mamadou lamine ba <laminba2003@gmail.com>
2020-05-02 11:36:39 +0000

    corrections

--- models/clients.js
+++ models/clients.js
@@ -1,37 +1,6 @@
+// une classe est une structure qui definit les attributs d'un objet
 export class Clients {
-    code;
-    nom: 
-
-
+    nom;
+    prenom;
+    adresse;
 }
-
-
-// format JSON à adapter pour l'écrire sous forme de class
-
-
-{
-    "Nom" :"Thomson",
-    "Prenom" : "Andora",
-    "Statut": "Cliente",
-    "Pays" :"Brésil",
-    "Membre Premium" : "Oui"
-   
-  }
-
-  {
-    "Nom" :"Loan",
-    "Prenom" : "Lindsey",
-    "Statut": "Cliente",
-    "Pays" :"Etats Unis",
-    "Membre Premium" : "Non"
-  }
-
-
-  {
-    "Nom" :"Foris",
-    "Prenom" : "Gabriel",
-    "Statut": "Client",
-    "Pays" :"Ecosse",
-    "Membre Premium" : "Oui"
-   
-  }
