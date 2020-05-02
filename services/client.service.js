9da390238db0100adc3ebdf3bbb096eef5f094ff
mamadou lamine ba <laminba2003@gmail.com>
2020-05-02 12:06:11 +0000

    corrections

--- services/client.service.js
+++ services/client.service.js
@@ -1,18 +1,18 @@
 import { Client } from "../models/client";
 
-export class PaysService {
+export class ClientService {
     
-    getClient() {
-        const client = [];
+    getClients() {
+        const clients = [];
         const client1 = new Client();
         client1.nom = "thomson";
         client1.prenom = "Gabriel";
-        client.push(client1);
+        clients.push(client1);
         const client2 = new Client();
         client2.nom = "Ford";
         client2.prenom = "Taraji";
-        client.push(client2);
-        return client;
+        clients.push(client2);
+        return clients;
     }
 
 }
