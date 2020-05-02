20244dc1349d33da1be42ca4174db9f83154cc15
maremend <mareme.ndoye@hotmail.fr>
2020-05-02 13:33:08 +0200

    update

---
+++ models/clients.js
@@ -0,0 +1,37 @@
+export class Clients {
+    code;
+    nom: 
+
+
+}
+
+
+// format JSON à adapter pour l'écrire sous forme de class
+
+
+{
+    "Nom" :"Thomson",
+    "Prenom" : "Andora",
+    "Statut": "Cliente",
+    "Pays" :"Brésil",
+    "Membre Premium" : "Oui"
+   
+  }
+
+  {
+    "Nom" :"Loan",
+    "Prenom" : "Lindsey",
+    "Statut": "Cliente",
+    "Pays" :"Etats Unis",
+    "Membre Premium" : "Non"
+  }
+
+
+  {
+    "Nom" :"Foris",
+    "Prenom" : "Gabriel",
+    "Statut": "Client",
+    "Pays" :"Ecosse",
+    "Membre Premium" : "Oui"
+   
+  }
